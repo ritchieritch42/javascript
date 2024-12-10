68b57adb4e09c0e0f69e77fe89f8e44eebe4be70
Caleb <ritchie.caleb42@gmail.com>
2024-12-10 16:10:51 -0600

    teasing out copyWithin more

--- Oreilly/JS The Def Guide/copyWithin.js
+++ Oreilly/JS The Def Guide/copyWithin.js
@@ -1,4 +1,29 @@
-myArray = [1, 2, 3, 4, 5];
+ogArray = [1, 2, 3, 4, 5];
+
+myArray = [...ogArray];
 myArray.forEach(() => {
     console.log(myArray.copyWithin(1));
+});
+
+console.log('\n');
+
+mySecondArray = [...ogArray];
+myArray.forEach(() => {
+    console.log(mySecondArray.copyWithin(2));
+});
+
+console.log('\n');
+
+myThirdArray = [...ogArray];
+myArray.forEach(() => {
+    console.log(myThirdArray.copyWithin(3));
+});
+
+console.log('\n');
+
+myFourthArray = [...ogArray];
+console.log(myFourthArray);
+
+myFourthArray.forEach(() => {
+    console.log(myFourthArray.copyWithin(4));
 });
