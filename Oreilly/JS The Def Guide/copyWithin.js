27278891e12996ba7549dcb2ac51d9916efc1412
Caleb <ritchie.caleb42@gmail.com>
2024-12-10 20:27:39 -0600

    copyWithin 2 & 3 arguments

--- Oreilly/JS The Def Guide/copyWithin.js
+++ Oreilly/JS The Def Guide/copyWithin.js
@@ -19,3 +19,16 @@ function testCopyWithin(array, index) {
 for (let i = 0, length = myArray.length; i <= length; i++) {
     testCopyWithin(myArray, i);
 }
+
+// The general copyWithin syntax is:
+// array.copyWithin(target, start, end);
+// Here's some other examples showing this
+// Target is required
+// The start default is 0 unless specified
+// The end default is length -1 unless specified
+// The sequence copied is start to end
+// The place pasted is target
+
+newArray = [...myArray];
+console.log(newArray.copyWithin(0, 5) + '\n') // 1, 2, 3, 4, 5
+console.log(newArray.copyWithin(2, 2, 5) + '\n') // 1, 2, 3, 4, 5
